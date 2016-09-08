95addf8b5ae6ed18c32e327b32ca80740b8ba864
Juan I Carrano <jc@eiwa.ag>
2016-09-08 11:31:36 -0300

    Add author, license, copyright.

--- setup.py
+++ setup.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python
+#
+#  Copyright 2016 EIWA S.A.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following disclaimer
+#    in the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of the  nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
 """
 setup.py file for RAW library
@@ -10,6 +39,9 @@ from distutils.command.build_ext import build_ext as _build_ext
 from distutils import spawn
 from distutils.version import LooseVersion
 import subprocess, re
+
+__author__ = [  "Juan Carrano <jc@eiwa.ag>"]
+__version__ = "0.1.1"
 
 def get_swig_executable():
     "Get SWIG executable"
@@ -51,9 +83,21 @@ libraw_wrapper = Extension('raw._libraw',
 
 setup (name = 'raw',
        version = '0.1.1',
+       description = """SWIG based LibRaw bindings""",
+       long_description='Low level but pythonic bindings for libraw.',
+       classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics :: Capture :: Digital Camera',
+       ],
+       keywords='camera raw images photos photography libraw',
+       # url = ???????,
        author      = 'Juan I Carrano',
        author_email='jc@eiwa.ag',
-       description = """LibRaw Wrapper""",
+       license = 'BSD',
        ext_modules = [libraw_wrapper],
        packages = ["raw"],
        cmdclass = {"build_ext": Build_Ext_find_swig3}
